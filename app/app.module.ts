84d005c01c36e211db2b0c70ffa5cabb25a15998
kaye9 <kaye992@gmail.com>
2016-12-13 22:58:08 +0800

    angular2 note-C 用户输入

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { ClickMeComponent } from './click-me.component'
-import {KeyUpComponent_v1} from "./keyup.components";
+import {
+  KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_V3, KeyUpComponent_V4,
+  LitterTourComponent
+} from "./keyup.components";
+import {LoopbackComponent} from "./loop-back.component";
 //import语句之后，可以看到一个@NgModule装饰器修饰的类。
 //@NgModule装饰器将AppModule标记为 Angular 模块类（也叫NgModule类）。 @NgModule接受一个元数据对象，告诉 Angular 如何编译和启动应用。
 @NgModule({
   imports:      [ BrowserModule ],
   //imports — BrowserModule，这个和每个在浏览器中运行应用都需要它。
   //当应用需要模块的特性时，将其添加到imports数组中。
-  declarations: [ AppComponent,ClickMeComponent,KeyUpComponent_v1 ],
+  declarations: [ AppComponent,ClickMeComponent,KeyUpComponent_v1,LoopbackComponent,KeyUpComponent_v2,KeyUpComponent_V3,KeyUpComponent_V4,LitterTourComponent ],
   //declarations — 应用的唯一组件
   //每个组件必须在且仅在一个NgModule类中声明。
   //指令和管道 — 它们也必须被添加到declarations数组。
--- app/keyup.components.ts
+++ app/keyup.components.ts
@@ -15,3 +15,61 @@ export class KeyUpComponent_v1 {
     // this.values += event.key + ' | ';
   }
 }
+@Component({
+  selector: 'key-up2',
+  template: `
+<!--在标识符前加上井号 (#) 就能声明一个模板引用变量。-->
+    <input #box (keyup)="onKey(box.value)">
+    <p>{{values}}</p>
+  `
+})
+export class KeyUpComponent_v2 {
+  values = '';
+  onKey(value: string) {
+    this.values += value + ' | ';
+  }
+}
+
+@Component({
+  selector: 'key-up3',
+  template: `
+    <input #box (keyup.enter)="onEnter(box.value)" >
+    <p>{{value}}</p>
+  `
+})
+export class KeyUpComponent_V3{
+  value = '';
+  onEnter(value:string){ this.value = value; }
+}
+
+@Component({
+  selector:'key-up4',
+  template: `
+    <input #box (keyup.enter)="update(box.value)" (blur)="update(box.value)">
+    <p>{{value}}</p>
+  `
+})
+export  class KeyUpComponent_V4{
+  value = '';
+  update(value:string){ this.value = value; }
+}
+
+@Component({
+  selector:'litter-tour',
+  template:`
+<!--第一句调用addHero。第二句newHero.value=''在添加新英雄到列表中后清除输入框。-->
+    <input #newHero (keyup.enter)="addHero(newHero.value); newHero.value=''" (blur)="addHero(newHero.value); newHero.value='' "  />
+    <button (click)="addHero(newHero.value)">add</button>
+    <ul>
+      <li *ngFor="let hero of heroes">{{ hero }}</li>    
+    </ul>
+  `
+})
+export class LitterTourComponent{
+  heroes = ['a','b','c'];
+  addHero(newHero:string){
+    if(newHero){
+      this.heroes.push(newHero);
+    }
+  }
+}
